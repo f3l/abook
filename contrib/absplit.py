ffee02d340b7f78c803bcd1eb827a5c941b8e23c
Dominik Heidler <dominik@heidler.eu>
2020-01-08 11:06:45 +0100

    Prevent ffmpeg from stealing stdin
    And some general improvements

--- contrib/absplit.py
+++ contrib/absplit.py
@@ -13,7 +13,7 @@ move_split_into_silence = True
 
 total_length = float(subprocess.check_output(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', input_file]))
 
-print("Total length: %f" % total_length)
+print("Total length: %d sec" % total_length)
 
 chapter_count = 1
 
@@ -28,19 +28,20 @@ def save_chapter(start, end=None):
 	if end:
 		cmd.extend(['-to', str(end)])
 	cmd.extend([os.path.join(output_folder, '%03d.%s' % (chapter_count, output_ext))])
-	print('=> Track: %03d.%s' % (chapter_count, output_ext))
+	tracklength = (end - start) if end else (total_length - start)
+	print('=> Track: %03d.%s (length: %d sec)' % (chapter_count, output_ext, tracklength))
 	subprocess.check_output(cmd, stderr=subprocess.DEVNULL)
 	chapter_count += 1
 
-p = subprocess.Popen(['ffmpeg', '-i', input_file, '-af', 'silencedetect=n=-50dB:d=%i' % silence_len, '-y', '-f', 'opus', '/dev/null'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-last_silence_start = 0
-for line in iter(p.stderr.readline, b''):
-	if b'silence_start:' in line:
-		silence_start = float(line.split(b'silence_start: ')[1])
-		print("   Progress: %f" % ((100*silence_start)/total_length))
-		#print("   Silence: %f" % silence_start)
-		if (last_silence_start + 300) <= silence_start:
-			# Create a track
-			save_chapter(last_silence_start, silence_start)
-			last_silence_start = silence_start
-save_chapter(last_silence_start)
+with subprocess.Popen(['ffmpeg', '-i', input_file, '-af', 'silencedetect=n=-50dB:d=%i' % silence_len, '-y', '-f', 'opus', '/dev/null'], stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, stdin=subprocess.PIPE) as p:
+	last_silence_start = 0
+	for line in iter(p.stderr.readline, b''):
+		if b'silence_start:' in line:
+			silence_start = float(line.split(b'silence_start: ')[1])
+			print("   Progress: %.2f%%" % ((100*silence_start)/total_length))
+			#print("   Silence: %f" % silence_start)
+			if (last_silence_start + 300) <= silence_start:
+				# Create a track
+				save_chapter(last_silence_start, silence_start)
+				last_silence_start = silence_start
+	save_chapter(last_silence_start)
